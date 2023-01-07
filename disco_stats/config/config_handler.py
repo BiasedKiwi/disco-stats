634ee6f34b7c64f774c73548ab583fc65cb9dc17
BiasedKiwi <alexandre1920911@gmail.com>
2023-01-07 11:01:08 +0100

    Fix deepsource issue

--- disco_stats/config/config_handler.py
+++ disco_stats/config/config_handler.py
@@ -4,6 +4,6 @@ import yaml
 
 def get_raw_config(path: str = "./disco_stats/config/config.yaml"):
     """Return a raw dict of the config file (using `yaml.load`)"""
-    with open(path, encoding="utf-8") as file:
+    with open(path, encoding="utf-8") as file:  # skipcq: PYL-W0622
         _dict = yaml.safe_load(file)
         return _dict
